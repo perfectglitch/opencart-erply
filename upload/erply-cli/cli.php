9f0ba6a7163d8614c069bd5f524b5c73071d2f78
Stanislav Gaistruk <stanislav.gaistruk@gmail.com>
2019-02-25 23:13:28 +0100

    cli dir htaccess , refactor cli, fix undefined var, password placeholder

--- upload/erply-cli/cli.php
+++ upload/erply-cli/cli.php
@@ -1,31 +1,31 @@
 <?php
 
-if (!is_cli()) {
-	header('Location: /');
-	exit;
-} elseif (!isset($argv[1])) {
-	echo "Action not specified. Usage: php-cli cli.php [erply_action_name]\n";
-	exit;
-}
+require_cli();
+require_action();
 
-// Setup
-define('VERSION', '3.0.2.0');
-if (is_file('../admin/config.php')) {
-	require_once('../admin/config.php');
-}
-require_once(DIR_SYSTEM . 'startup.php');
+init_oc();
+start("$argv[1]");
 
-// Set the action to execute
-$erply_action = $argv[1];
-
-// Start
-start("$erply_action");
-
-function is_cli()
+function require_cli()
 {
 	if (defined('STDIN') && php_sapi_name() === 'cli' && empty($_SERVER['REMOTE_ADDR']) && !isset($_SERVER['HTTP_USER_AGENT']) && count($_SERVER['argv']) > 0 && !array_key_exists('REQUEST_METHOD', $_SERVER)) {
-		return true;
+		header('Location: /');
+		exit;
 	}
+}
 
-	return false;
+function require_action(){
+	if (!isset($argv[1])) {
+		echo "Action not specified. Usage: php-cli cli.php [erply_action_name]\n";
+		exit;
+	}
+}
+
+function init_oc(){
+	define('VERSION', '3.0.2.0');
+	
+	if (is_file('../admin/config.php')) {
+		require_once('../admin/config.php');
+	}
+	require_once(DIR_SYSTEM . 'startup.php');
 }
